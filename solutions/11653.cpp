2ef653b47de277b7458727d8ffd7552c9e2517fa
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2022-03-10 01:11:33 +0900

    Update 11653.cpp

--- solutions/11653.cpp
+++ solutions/11653.cpp
@@ -7,7 +7,7 @@ int main(void) {
     int n;
     cin >> n;
     while (n > 1) {
-        for (int i = 2; i <=n; i++) {
+        for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
                 cout << i << '\n';
                 n /= i;
